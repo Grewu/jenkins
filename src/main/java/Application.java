c5eda01dc415a9977d75a440d2f036ce53ad9f57
Pavel Gurevich <pavelgurevichwork@gmail.com>
2024-09-04 18:01:55 +0300

    fix imports

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
